377047a38f6020757977c85e6edd4234e6698aff
Alisa Herr <alisa@unitymakes.us>
2018-08-04 16:32:07 -0400

    Add bb plugin

---
+++ wp-content/plugins/bb-plugin/includes/updater-config.php
@@ -0,0 +1,10 @@
+<?php
+
+if ( class_exists( 'FLUpdater' ) ) {
+	FLUpdater::add_product(array(
+		'name'      => 'Beaver Builder Plugin (Standard Version)',
+		'version'   => '2.1.1.3',
+		'slug'      => 'bb-plugin',
+		'type'      => 'plugin',
+	));
+}
